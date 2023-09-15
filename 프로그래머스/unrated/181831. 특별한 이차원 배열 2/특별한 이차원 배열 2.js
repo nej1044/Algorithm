20ad3d45b412beb56dc67a0705c768c4294029bd
Eunjeong Noh <82998038+nej1044@users.noreply.github.com>
2023-09-15 23:33:01 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    for(let i=0; i<=arr.length/2; i++){
+        for(let j=0; j<=arr.length/2; j++){
+            if(arr[i][j] !== arr[j][i]) return 0
+        }
+    }
+    return 1
+}
