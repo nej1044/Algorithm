3734b33c434e5185e511826adccaacfbbba38803
Eunjeong Noh <82998038+nej1044@users.noreply.github.com>
2023-09-16 00:24:12 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,11 @@
+const numbers = ["zero", "one", "two", "three", "four", "five", "six",
+                 "seven", "eight", "nine"];
+
+function solution(s) {
+    for(let i=0; i<numbers.length; i++){
+        const regExp = new RegExp(numbers[i], 'g')
+        s = s.replace(regExp, i)
+    }
+    
+    return Number(s)
+}
