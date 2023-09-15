56a5cc2d43ce166df2df52760c7276fb69ad7067
Eunjeong Noh <82998038+nej1044@users.noreply.github.com>
2023-09-15 23:56:33 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,13 @@
+function solution(seoul) {
+    const x = seoul.indexOf("Kim")
+    return `김서방은 ${x}에 있다`
+    // let x = 0; // 김서방의 위치(인덱스) 값을 저장
+    
+//     for(let i=0; i<seoul.length; i++){
+//        if(seoul[i]==="Kim") {
+//           return `김서방은 ${i}에 있다` 
+//            // x = i;
+//            // break;
+//         }
+//     }
+}
