4d660498e2ddb26511175c40926c4a9270bfd1df
Eunjeong Noh <82998038+nej1044@users.noreply.github.com>
2026-02-04 21:49:08 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,6 +1,6 @@
 function solution(n) {
     let answer = 0;
-    for(let i=0; i<=Math.sqrt(n); i++) {
+    for(let i=1; i<=Math.sqrt(n); i++) {
         if(n%i === 0) {
             answer++;
             if(n/i !== i) answer++
